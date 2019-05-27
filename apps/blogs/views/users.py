83a632071950cbfc457ca96eb57b97164794d6f0
Justin Tungul <justin.tungul@gmail.com>
2019-05-26 22:54:10 -0700

    Add login page and single index.html dynamic routing

--- apps/blogs/views/users.py
+++ apps/blogs/views/users.py
@@ -8,13 +8,31 @@ def index(request):
     #     'customers': Customer.objects.all(),
     #     'projects': Project.objects.all()
     # }
-    return render(request, 'blogs/pages/index.html')
+    context = {
+        'route': 'blogs/pages/home.html'
+    }
+    return render(request, 'blogs/index.html', context)
 
 def about(request):
-    return render(request, 'blogs/pages/about.html')
+    context = {
+        'route': 'blogs/pages/about.html'
+    }
+    return render(request, 'blogs/index.html', context)
 
 def post(request):
-    return render(request, 'blogs/pages/post.html')
+    context = {
+        'route': 'blogs/pages/post.html'
+    }
+    return render(request, 'blogs/index.html', context)
 
 def contact(request):
-    return render(request, 'blogs/pages/contact.html')
+    context = {
+        'route': 'blogs/pages/contact.html'
+    }
+    return render(request, 'blogs/index.html', context)
+
+def login(request):
+    context = {
+        'route': 'blogs/pages/login.html'
+    }
+    return render(request, 'blogs/index.html', context)
